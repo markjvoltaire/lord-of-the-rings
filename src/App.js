902c2ec26250324719e14df2f2f4585756367df8
Mark Voltaire <markjvoltaire@gmail.com>
2021-12-14 11:33:42 -0500

    commit for submission

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { BrowserRouter, NavLink } from 'react-router-dom';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
 
 import CharacterList from './components/Characters/CharacterList';
 import FilmList from './components/Films/FilmList';
@@ -15,34 +15,19 @@ function App() {
   }, []);
 
   const getFilms = async () => {
-    // Add your code here!
-    // 1. Get data using fetch from https://the-one-api.dev/v2/movie/ (don't forget to set your header!)
-    // 2. Transform the response so that films contains nested arrays of:
-    //   - the film's title
-    //   - the film's title "slugified" i.e. in all lower case, with words separated with dashes,
-    //   - the box office total
-    //   - academy award nominations
-    // NOTE: make sure you look at the response from the server - it may not be consistent
-    // [["The Lord of the Rings Series", "the-lord-of-the-rings-series", 2917, 30 ], ["The Hobbit Series", "the-hobit-series", 2932, 7]...]
+    const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/films`, {
+      headers: {
+        apikey: process.env.REACT_APP_SUPABASE_KEY,
+        Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
+      },
+    });
 
-    // 3. Set the resulting transformation as state using setFilms
-    // 4. You'll know it works if the films show up on the page
+    const data = await resp.json();
+    setFilms(data);
     return [];
   };
 
   const getCharacters = async () => {
-    // Add your code here!
-    // 1. Get data using fetch from https://the-one-api.dev/v2/character/
-    // 2. Update the response data with the key `dates` which is a combination of
-    //    the `birth` key and the `death key` separated with a dash. If neither date
-    //    is provided, it should hold the string 'Unknown'
-    //    [
-    //       {name: 'Adanel', birth: "", death: "", dates: "Unknown", ...},
-    //       {name: 'Adrahil I', birth: "Before , TA 1944", death: "Late , Third Age", dates: "Before , TA 1944 - Late , Third Age", ...},
-    //       {name: 'Adrahil II', birth: "TA 2917", death: "TA 3010, dates: "TA 2917 - TA 3010", ...},
-    //    ]
-    // 3. Set the resulting transformation as state using setCharacters
-    // 4. You'll know it works if the characters show up on the page
     return [];
   };
 
@@ -53,11 +38,16 @@ function App() {
           <NavLink to="/films" data-testid="film-link">
             Films
           </NavLink>
+
           <NavLink to="/characters" data-testid="char-link">
             Characters
           </NavLink>
         </header>
-        {/* ADD YOUR ROUTES HERE */}
+
+        <Switch>
+          <Route exact path="/films" component={FilmList} />
+          <Route exact path="/characters" component={CharacterList} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
